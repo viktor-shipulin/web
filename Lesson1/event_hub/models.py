5cf678d6160612ef09d01fff778be48f48176fcf
viktor-shipulin <130042307+viktor-shipulin@users.noreply.github.com>
2026-09-10 17:00:08 +0600

    migration and creating event model

--- Lesson1/event_hub/models.py
+++ Lesson1/event_hub/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Event(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    event_date = models.DateField()
